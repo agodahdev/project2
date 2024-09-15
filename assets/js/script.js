17e16674c58fd92472167fbbe43210458b302811
agodahdev Goddah <godahahmed@gmail.com>
2024-09-15 22:29:49 +0000

    Add the first few questions of the quiz on js

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// Quiz questions and answers (25 questions)
+const questions = [
+{
+    question: "Who is known as the 'king of Football'?",
+    answers: [
+        { text: "Pele", correct: true},
+        { text: "Maradona", correct: false},
+        { text: "Lionel Messi", correct: false},
+        { text: "Cristiano Ronaldo", correct: false},
+    ]
+},
+{
+    question: "Which country has won the most FIFA World Cups'?",
+    answers: [
+        { text: "Brazil", correct: true},
+        { text: "Germany", correct: false},
+        { text: "Argentina", correct: false},
+        { text: "Italy", correct: false},
+    ]
+},
+{
+    question: "Who holds the record for most international goals in football'?",
+    answers: [
+        { text: "Cristiano Ronaldo", correct: true},
+        { text: "Lionel Messi", correct: false},
+        { text: "Pele", correct: false},
+        { text: "Ali Daei", correct: false},
+    ]
+},
+
+// Premier League Questions
+{
+    question: "Which player has the most Premier League goals in a single season?",
+    answers: [
+        { text: "Alan Shearer", correct: true},
+        { text: "Mohamed Salah", correct: false},
+        { text: "Thierry Henry", correct: false},
+        { text: "Sergio Agüero", correct: false},
+    ]
+},
+
+
+
+]
